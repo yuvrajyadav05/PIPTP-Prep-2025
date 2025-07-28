0fcff4219c116574a772ea3a4e1e00783d203f2d
yuvrajyadav05 <162972656+yuvrajyadav05@users.noreply.github.com>
2025-07-29 00:51:49 +0530

    Create Second.m

---
+++ Week1/Day1/Second.m
@@ -0,0 +1,10 @@
+Q2: Consider the pseudocode mentioned below. For how many times, the while loop will be executed?
+
+1. Integer a  
+2. Set a = 1  
+3. while (a < 5)  
+4.     a = a + 2  
+5. end while  
+6. Print a
+Options A) 2 B)5 C)1 D)3
+Correct option: A)2
